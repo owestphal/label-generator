6f155229c0469cfe4f21afcbd0c95a595a804a9b
Oliver Westphal <o_westphal at posteo.net>
2021-09-20 20:13:46 +0200

    calibration support

--- src/pdf/Main.hs
+++ src/pdf/Main.hs
@@ -9,14 +9,19 @@ main :: IO ()
 main = void $ do
   args <- getArgs
   case args of
+    -- calibration page
+    ["--calibration-page",target] -> do
+      path <- createCalibrationPage
+      copyFile path target
+    -- set calibration
+    ["--print-settings", top, bottom, left, right] -> do
+      saveCalibration $ PrinterSetting (read top) (read bottom) (read left) (read right)
+    -- create labels
     [year,f,l,n,target] -> do
       printerSettings <- loadCalibration
       let
         number = read n
         conf = offsetLabelPosition indexConf{numberOfLabels=number} 0 0
       path <- createLabelPdf (Settings conf printerSettings) $ parseLabelData (RangeLabel (read year) (read f) (read l) True)
-      copyFile path target
-    ["--calibration-page",target] -> do
-      path <- createCalibrationPage
       copyFile path target
     _ -> putStrLn "unknown parameters"
